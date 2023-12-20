166b2d75ed0e34b435d9b5c4274c61f14f067001
Antoine Thach <antoine.thach@gmail.com>
2023-12-20 10:45:45 +0100

    Adding Sample.Music.Instrument.Models

---
+++ Sample-Music-Instrument.API/Sample-Music-Instrument.Models/InstrumentType.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Sample_Music_Instrument.Models
+{
+    public class InstrumentType
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+    }
+}
